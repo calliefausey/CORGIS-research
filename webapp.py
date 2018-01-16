61d738670c02dbec998f9d6273fb41ab5d68fe57
calliefausey <31317882+calliefausey@users.noreply.github.com>
2018-01-16 13:00:21 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -29,9 +29,9 @@ def render_main():
 def render_page1():
     with open('drugs (1).json') as rates_data:
         rates = json.load(rates_data)
-    if 'state' in request.args:
-        return render_template('page1.html', options = get_state_options(rates), total = totals(request.args['state']), state = request.args['state'])
-    return render_template('page1.html', options = get_state_options(rates))
+    if 'state' and 'year' in request.args:
+        return render_template('page1.html', stateoptions = get_state_options(rates), yearoptions = get_year_options(rates), total = totals((request.args['state']), (request.args['year'])), state = request.args['state'], year = request.args['year'])
+    return render_template('page1.html', stateoptions = get_state_options(rates), yearoptions = get_year_options(rates))
 
 @app.route("/p2")
 def render_page2():
@@ -43,21 +43,29 @@ def render_page3():
 
 def get_state_options(rates):
     states = []
-    options = ""
+    stateoptions = ""
     for r in rates:
         if r["State"] not in states:
             states.append(r["State"])
             options += Markup("<option value=\"" + r["State"] + "\">" + r["State"] + "</option>")
-    return options
+    return stateoptions
+    
+def get_year_options(rates):
+    years = []
+    yearoptions = ""
+    for r in rates:
+        if r["Year"] not in years:
+            states.append(r["Year"])
+            options += Markup("<option value=\"" + r["Year"] + "\">" + r["Year"] + "</option>")
+    return yearoptions
 
-def totals(state):
+def totals(state, year):
     with open('drugs (1).json') as corgis_data:
         rates = json.load(corgis_data)
     total = 0
     for r in rates:
-        if state == r["State"]:
-            print(r["Totals"]["Illicit Drugs"]["Abuse Past Month"]["12-17"])    
-	    total += r["Totals"]["Illicit Drugs"]["Abuse Past Month"]["12-17"]
+        if state == r["State"] and year == r["Year"]:  
+            total += r["Totals"]["Illicit Drugs"]["Abuse Past Month"]["12-17"]
     return total
 	
 
